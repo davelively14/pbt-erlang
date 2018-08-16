cf341e4a240e5dcfd89806aaa8c79e89855e7fa2
davelively14 <dave.lively@salesloft.com>
2018-08-15 22:46:19 -0400

    Final

---
+++ test/prop_foundations.erl
@@ -0,0 +1,12 @@
+-module(prop_foundations).
+-include_lib("proper/include/proper.hrl").
+
+prop_test() ->
+    ?FORALL(Type, term(),
+        begin
+            boolean(Type)
+        end).
+
+boolean(_) -> true.
+
+mytype() -> term().
